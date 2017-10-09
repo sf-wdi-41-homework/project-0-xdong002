e1d5883f1582f1c19a10e1270dabe1f5217cc4f4
unknown <xxx419168445@gmail.com>
2017-10-09 09:59:16 -0700

    added comment for the codes

--- js/app.js
+++ js/app.js
@@ -1,36 +1,50 @@
 $(function() {
 
   var anim_id;
+  //------Score------
   var score = $('#score');
   var score_counter = 1;
-
+  //------Player1 and player2------
   var car = $('#car');
+  var car_width = parseInt(car.width());
+  var car_height = parseInt(car.height());
+  var car1 = $('#car1');
+  var car1_width = parseInt(car1.width());
+  var car1_height = parseInt(car1.height());
+  //-----AI cars and lines-----
   var car2 = $('#car2');
   var car3 = $('#car3');
   var car4 = $('#car4');
   var line_1 = $('#line_1');
   var line_2 = $('#line_2');
   var line_3 = $('#line_3');
-  var gameBoard = $('#gameBoard');
-
-  var game_over = false;
-
+  //------controls for player1------
   var move_right = false;
   var move_left = false;
   var move_up = false;
   var move_down = false;
-
+  //------controls for player2------
+  var move_right1 = false;
+  var move_left1 = false;
+  var move_up1 = false;
+  var move_down1 = false;
+  //------game board-----
+  var gameBoard = $('#gameBoard');
   var gameBoard_left = parseInt(gameBoard.css('left'));
   var gameBoard_width = parseInt(gameBoard.width());
   var gameBoard_height = parseInt(gameBoard.height());
-  var car_width = parseInt(car.width());
-  var car_height = parseInt(car.height());
 
-
+  //-----game speed-----
   var speed = 2;
   var line_speed = 5;
 
+
+  var restart_btn = $('#startButton');
+  var game_over = false;
+
+//-----functions for control the player1's car------
   $(document).on('keydown', function(e) {
+      e.preventDefault();
       if (game_over === false) {
           var key = e.keyCode;
           if (key === 37 && move_left === false) {
@@ -91,13 +105,79 @@ $(function() {
           move_down = requestAnimationFrame(down);
       }
   }
+
+  //------functions for control the player2's car------
+  $(document).on('keydown', function(e) {
+      if (game_over === false) {
+          var key = e.keyCode;
+          if (key === 65 && move_left1 === false) {
+              move_left1 = requestAnimationFrame(left1);
+          } else if (key === 68 && move_right1 === false) {
+              move_right1 = requestAnimationFrame(right1);
+          } else if (key === 87 && move_up1 === false) {
+              move_up1 = requestAnimationFrame(up1);
+          } else if (key === 83 && move_down1 === false) {
+              move_down1 = requestAnimationFrame(down1);
+          }
+      }
+  });
+
+  $(document).on('keyup', function(e) {
+      if (game_over === false) {
+          var key = e.keyCode;
+          if (key === 65) {
+              cancelAnimationFrame(move_left1);
+              move_left1 = false;
+          } else if (key === 68) {
+              cancelAnimationFrame(move_right1);
+              move_right1 = false;
+          } else if (key === 87) {
+              cancelAnimationFrame(move_up1);
+              move_up1 = false;
+          } else if (key === 83) {
+              cancelAnimationFrame(move_down1);
+              move_down1 = false;
+          }
+      }
+  });
+
+  function left1() {
+      if (game_over === false && parseInt(car1.css('left')) > 0) {
+          car1.css('left', parseInt(car1.css('left')) - 5);
+          move_left1 = requestAnimationFrame(left1);
+      }
+  }
+
+  function right1() {
+      if (game_over === false && parseInt(car1.css('left')) < gameBoard_width - car1_width) {
+          car1.css('left', parseInt(car1.css('left')) + 5);
+          move_right1 = requestAnimationFrame(right1);
+      }
+  }
+
+  function up1() {
+      if (game_over === false && parseInt(car1.css('top')) > 0) {
+          car1.css('top', parseInt(car1.css('top')) - 3);
+          move_up1 = requestAnimationFrame(up1);
+      }
+  }
+
+  function down1() {
+      if (game_over === false && parseInt(car1.css('top')) < gameBoard_height - car1_height) {
+          car1.css('top', parseInt(car1.css('top')) + 3);
+          move_down1 = requestAnimationFrame(down1);
+      }
+  }
+
 //-----------------------------------------------------------------------------------
 
   anim_id = requestAnimationFrame(repeat);
 
+//-----the line and the AI cars will always repeat itself------
   function repeat() {
-      if (collision(car, car2) || collision(car, car3) || collision(car, car4)) {
+      if ((collision(car, car2) || collision(car, car3) || collision(car, car4)) || (collision(car1, car2) || collision(car1, car3) || collision(car1, car4))) {
           stop_the_game();
+          gameOver() ;
           return;
       }
 
@@ -105,6 +185,10 @@ $(function() {
 
       if (score_counter % 20 == 0) {
           score.text(parseInt(score.text()) + 1);
+      }
+      if (score_counter % 500 == 0) {
+          speed++;
+          line_speed++;
       }
 
       car_down(car2);
@@ -136,6 +220,12 @@ $(function() {
       line.css('top', line_current_top + line_speed);
   }
 
+  restart_btn.click(function() {
+      location.reload();
+  });
+
+  //------Game over function(make everything stoped)------
+
   function stop_the_game() {
       game_over = true;
       cancelAnimationFrame(anim_id);
@@ -146,10 +236,13 @@ $(function() {
   }
 
 
+//-----show the words when game is over------
+function gameOver() {
+  $('.restartBox').append('<h1>Game Over!</h1>')
+  $('.restartBox').append('<h3><------- Press the start game button to restart the game!</h3>')
+}
 
-
-
-//----------------------------------------------------------------------
+//------function for calculate the cars collision------
 function collision($div1, $div2) {
     var x1 = $div1.offset().left;
     var y1 = $div1.offset().top;
@@ -167,9 +260,6 @@ function collision($div1, $div2) {
     if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
     return true;
 }
-
-
-
 
 
 });
